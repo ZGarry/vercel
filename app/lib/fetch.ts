8712c89826c250385457c4b3e43b34eed176c49f
ZGarry <1160564525@qq.com>
2024-04-14 00:32:56 +0800

    完善url

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -2,7 +2,12 @@
 export const fpy = async (
     path: string
   ) => {
-    const res = await fetch(path)
+    const url =  process.env.VERCEL_ENV === 'production'
+    ? process.env.VERCEL_URL
+    : '0.0.0.0:5328';
+    console.log(process.env.VERCEL_ENV)
+
+    const res = await fetch("http://" + url + path)
     let result = await res.text();
     return result;
   };
