da960bee9c0f3a8d477fb15799ff5c111c53988d
ZGarry <1160564525@qq.com>
2024-04-13 23:17:06 +0800

    完善api读取1

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -2,7 +2,7 @@
 export const fpy = async (
     path: string
   ) => {
-    const res = await fetch('http://0.0.0.0:5328' + path)
+    const res = await fetch('http://0.0.0.0' + path)
     let result = await res.text();
     return result;
   };
