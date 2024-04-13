850e570e0c934fa93754627414203f797657dcc1
ZGarry <1160564525@qq.com>
2024-04-13 23:28:13 +0800

    1

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -2,7 +2,7 @@
 export const fpy = async (
     path: string
   ) => {
-    const res = await fetch('http://localhost' + path)
+    const res = await fetch('http://localhost:5328' + path)
     let result = await res.text();
     return result;
   };
