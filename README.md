9d937f695f77aa435be1a952c34061c0403f07df
Garry <43488431+ZGarry@users.noreply.github.com>
2024-04-13 23:54:01 +0800

    完善对应内容

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ For more information, see the [course curriculum](https://nextjs.org/learn) on t
 策略：
 
 1. 一点点加flask部分能力
+2. 用github远程编辑器的好处，天然就是分项目的。一切都不在需要自己手动管理项目了。（本地一个个项目终于不用了）
--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -2,7 +2,7 @@
 export const fpy = async (
     path: string
   ) => {
-    const res = await fetch(path)
+    const res = await fetch("http://127.0.0.1:5328" + path)
     let result = await res.text();
     return result;
   };
